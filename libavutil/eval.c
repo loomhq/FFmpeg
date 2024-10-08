b68221a340ebf1c4df081f51a7e36b74bea5725b
Steve Hsieh <steven.hsieh@loom.com>
2024-10-08 15:36:49 -0700

    increment number of complex filters

--- libavutil/eval.c
+++ libavutil/eval.c
@@ -729,7 +729,7 @@ int av_expr_parse(AVExpr **expr, const char *s,
     *wp++ = 0;
 
     p.class      = &eval_class;
-    p.stack_index=200;
+    p.stack_index=300;
     p.s= w;
     p.const_names = const_names;
     p.funcs1      = funcs1;
