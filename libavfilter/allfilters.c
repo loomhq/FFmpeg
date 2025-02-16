1f74de1fc5d45a5ac20c85b8a9e1fa3f59a4861b
Jorge Estrada <jestrada@atlassian.com>
2025-02-16 15:33:24 -0800

    Only include pad_npp changes

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -547,8 +547,6 @@ extern const AVFilter ff_vf_xstack_vaapi;
 extern const AVFilter ff_vf_hstack_qsv;
 extern const AVFilter ff_vf_vstack_qsv;
 extern const AVFilter ff_vf_xstack_qsv;
-extern const AVFilter ff_vf_pad_vaapi;
-extern const AVFilter ff_vf_drawbox_vaapi;
 
 extern const AVFilter ff_vsrc_allrgb;
 extern const AVFilter ff_vsrc_allyuv;
@@ -570,7 +568,6 @@ extern const AVFilter ff_vsrc_openclsrc;
 extern const AVFilter ff_vsrc_qrencodesrc;
 extern const AVFilter ff_vsrc_pal75bars;
 extern const AVFilter ff_vsrc_pal100bars;
-extern const AVFilter ff_vsrc_perlin;
 extern const AVFilter ff_vsrc_rgbtestsrc;
 extern const AVFilter ff_vsrc_sierpinski;
 extern const AVFilter ff_vsrc_smptebars;
@@ -608,9 +605,9 @@ extern const AVFilter ff_avsrc_amovie;
 extern const AVFilter ff_avsrc_movie;
 
 /* those filters are part of public or internal API,
-* they are formatted to not be found by the grep
-* as they are manually added again (due to their 'names'
-* being the same while having different 'types'). */
+ * they are formatted to not be found by the grep
+ * as they are manually added again (due to their 'names'
+ * being the same while having different 'types'). */
 extern  const AVFilter ff_asrc_abuffer;
 extern  const AVFilter ff_vsrc_buffer;
 extern  const AVFilter ff_asink_abuffer;
